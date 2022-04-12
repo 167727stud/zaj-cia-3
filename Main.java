86a3802e6e05652b253b3651b7f930b556a567b3
167727 <100698238+167727stud@users.noreply.github.com>
2022-04-12 12:47:17 +0000

    3.1

--- Main.java
+++ Main.java
@@ -1,18 +1,54 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+{ 
+   System.out.print( "1:wpisz imie nazwisko i wiek");
+  System.out.print(System.lineSeparator());
+   System.out.print( "2:wypisz  studentów");
+  System.out.print(System.lineSeparator());
+  Service1 s = new Service1();
+  int mem =1;
+  Scanner myInput = new Scanner( System.in );
+  mem = myInput.nextInt();
+    
+   switch(mem)
+  {
+    case 1:
+    
+   
     try {
-      Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
 
-      var students = s.getStudents();
-      for(Student current : students) {
+      int wiek;
+      Scanner myObj = new Scanner(System.in);
+      String wpis = myObj.nextLine();
+      Scanner myObj2 = new Scanner(System.in);
+      String wpis2 = myObj.nextLine();
+      Scanner myInput2 = new Scanner( System.in );
+      wiek = myInput2.nextInt();
+
+     s.addStudent(new Student(wpis,wpis2,wiek));
+            }
+            
+      catch (IOException e) {
+
+
+        }
+    break;
+    case 2:
+       
+      try {
+            var students = s.getStudents();
+      for(Student current : students) 
+      {
         System.out.println(current.ToString());
       }
-    } catch (IOException e) {
+            }
+      catch (IOException e) {
 
     }
+    break;
   }
+}
 }
--- Student.java
+++ Student.java
@@ -2,23 +2,26 @@ public class Student {
 
   private String Name;
   private int Age;
+  private String Sur;
 
-  public Student(String name, int age) {
+  public Student(String name ,String sur, int age) {
     Name = name;
     Age = age;
+    Sur = sur;
   }
 
   public String GetName() {return Name;}
   public int GetAge() {return Age;}
+  public String GetSur() {return Sur;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + Sur + " " + Integer.toString(Age);
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
     if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+      return new Student("Parse Error","Parse Error", -1);
+    return new Student(data[0],data[0], Integer.parseInt(data[1]));
   }
 }
